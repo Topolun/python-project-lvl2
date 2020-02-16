6d03ac98cddad85007ee84f4136921c284ff173b
Topolun <vik83tu@yandex.ru>
2020-02-16 20:48:50 +0400

    шаг 2 проекта

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('-f', '--format', type=str, help='set format of output')
 parser.add_argument('first_file', type=str, help='first file for check')
 parser.add_argument('second_file', type=str, help='second file for check')
 args = parser.parse_args()
