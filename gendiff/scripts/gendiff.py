4f4cfebe9961b7836a3087650125b6bec6be0ba0
Topolun <vik83tu@yandex.ru>
2020-02-16 20:35:24 +0400

    Первые данные для 2 проекта

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str, help='first file for check')
+parser.add_argument('second_file', type=str, help='second file for check')
+args = parser.parse_args()
+
+def main():
+    return print('srart ok')
+
+
+if __name__ == '__main__':
+    main()
